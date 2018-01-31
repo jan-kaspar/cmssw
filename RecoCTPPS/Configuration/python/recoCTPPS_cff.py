cf6edd3134dcfa4a65e8d37fd9f438dd189325a8
Jan Kaspar <jan.kaspar@cern.ch>
2018-01-31 17:04:34 +0100

    Geometry and alignments defined only once - top level config.

--- RecoCTPPS/Configuration/python/recoCTPPS_cff.py
+++ RecoCTPPS/Configuration/python/recoCTPPS_cff.py
@@ -1,5 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+# geometry
+from Geometry.VeryForwardGeometry.geometryRP_cfi import *
+
+# alignments
+from Geometry.VeryForwardGeometryBuilder.ctppsIncludeAlignments_cfi import *
+ctppsIncludeAlignments.RealFiles = cms.vstring("Alignment/CTPPS/data/RPixGeometryCorrections.xml")
+
+# reconstruction sequences
 from RecoCTPPS.TotemRPLocal.totemRPLocalReconstruction_cff import *
 from RecoCTPPS.TotemRPLocal.ctppsDiamondLocalReconstruction_cff import *
 from RecoCTPPS.TotemRPLocal.ctppsLocalTrackLiteProducer_cff import ctppsLocalTrackLiteProducer
