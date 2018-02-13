2f4f5730652e3a09953f176bbc9ffec50cc030a3
Jan Kaspar <jan.kaspar@cern.ch>
2018-02-13 16:27:20 +0100

    Process all components of shift and rotation.

--- Alignment/CTPPSTrackBased/src/CommonMethods.cc
+++ Alignment/CTPPSTrackBased/src/CommonMethods.cc
@@ -80,7 +80,8 @@ void CommonMethods::FactorRPFromSensorCorrections(const RPAlignmentCorrectionsDa
       TMatrixD B(N, 2), Vi(N, N);
       TVectorD M(N);
 
-      double S_rotz=0.;
+      double S_sh_z=0.;
+      double S_rot_x=0., S_rot_y=0., S_rot_z=0.;
 
       unsigned int idx = 0;
       for (const auto &senId : sensors)
@@ -93,8 +94,12 @@ void CommonMethods::FactorRPFromSensorCorrections(const RPAlignmentCorrectionsDa
         M(idx) = d2.dx * fullAlignments[senId].getShX() + d2.dy * fullAlignments[senId].getShY();
 
         Vi(idx, idx) = 1.;
+
+        S_sh_z += fullAlignments[senId].getShZ();
         
-        S_rotz += fullAlignments[senId].getRotZ();
+        S_rot_x += fullAlignments[senId].getRotX();
+        S_rot_y += fullAlignments[senId].getRotY();
+        S_rot_z += fullAlignments[senId].getRotZ();
 
         ++idx;
       }
@@ -105,28 +110,33 @@ void CommonMethods::FactorRPFromSensorCorrections(const RPAlignmentCorrectionsDa
 	  TVectorD th_B(2);
       th_B = BTViBi * BT * Vi * M;
 
-      double m_shx = th_B[0];
-      double m_shy = th_B[1];
-      double m_rotz = S_rotz / N;
+      const double m_sh_x = th_B[0];
+      const double m_sh_y = th_B[1];
+      const double m_sh_z = S_sh_z / N;
 
-      printf("    m_shx = %.3f, m_shy = %.3f, m_rotz = %.3f\n", m_shx, m_shy, m_rotz);
+      const double m_rot_x = S_rot_x / N;
+      const double m_rot_y = S_rot_y / N;
+      const double m_rot_z = S_rot_z / N;
 
-      factoredAlignments.addRPCorrection(rpId, RPAlignmentCorrectionData(m_shx, m_shy, 0., 0., 0., m_rotz));
+      printf("    m_sh_x = %.3f, m_sh_y = %.3f, m_sh_z = %.3f, m_rot_x = %.3f, m_rot_y = %.3f, m_rot_z = %.3f\n",
+        m_sh_x, m_sh_y, m_sh_z, m_rot_x, m_rot_y, m_rot_z);
+
+      factoredAlignments.addRPCorrection(rpId, RPAlignmentCorrectionData(m_sh_x, m_sh_y, m_sh_z, m_rot_x, m_rot_y, m_rot_z));
 
       // calculate residuals
       for (const auto &senId : sensors)
       {
         auto d2 = geometry.Get(senId).GetDirectionData(2);
 
-        const double de_s = d2.dx * (fullAlignments[senId].getShX() - m_shx) + d2.dy * (fullAlignments[senId].getShY() - m_shy);
+        const double de_s = d2.dx * (fullAlignments[senId].getShX() - m_sh_x) + d2.dy * (fullAlignments[senId].getShY() - m_sh_y);
 
         factoredAlignments.addSensorCorrection(senId, RPAlignmentCorrectionData(
           d2.dx * de_s,
           d2.dy * de_s,
-          0.,
-          0.,
-          0.,
-          fullAlignments[senId].getRotZ() - m_rotz
+          fullAlignments[senId].getShZ() - m_sh_z,
+          fullAlignments[senId].getRotX() - m_rot_x,
+          fullAlignments[senId].getRotY() - m_rot_y,
+          fullAlignments[senId].getRotZ() - m_rot_z
         ));
       }
     }
@@ -137,33 +147,48 @@ void CommonMethods::FactorRPFromSensorCorrections(const RPAlignmentCorrectionsDa
       // TODO: this approach ignores uncertainties/weights
 
       // extract mean shx, shy and rotz
-      double S_1=0., S_shx=0., S_shy=0., S_rotz=0.;
+      // TODO: add other components
+      double S_1=0.;
+      double S_sh_x=0., S_sh_y=0., S_sh_z=0.;
+      double S_rot_x=0., S_rot_y=0., S_rot_z=0.;
       for (const auto &senId : sensors)
       {
+        const auto &a = fullAlignments[senId];
+
         S_1 += 1.;
-        S_shx += fullAlignments[senId].getShX();
-        S_shy += fullAlignments[senId].getShY();
-        S_rotz += fullAlignments[senId].getRotZ();
+
+        S_sh_x += a.getShX();
+        S_sh_y += a.getShY();
+        S_sh_z += a.getShZ();
+
+        S_rot_x += a.getRotX();
+        S_rot_y += a.getRotY();
+        S_rot_z += a.getRotZ();
       }
     
-      double m_shx = S_shx / S_1;
-      double m_shy = S_shy / S_1;
-      double m_rotz = S_rotz / S_1;
+      const double m_sh_x = S_sh_x / S_1;
+      const double m_sh_y = S_sh_y / S_1;
+      const double m_sh_z = S_sh_y / S_1;
 
-      printf("    S_1 = %.3f, m_shx = %.3f, m_shy = %.3f, m_rotz = %.3f\n", S_1, m_shx, m_shy, m_rotz);
+      const double m_rot_x = S_rot_x / S_1;
+      const double m_rot_y = S_rot_y / S_1;
+      const double m_rot_z = S_rot_z / S_1;
 
-      factoredAlignments.addRPCorrection(rpId, RPAlignmentCorrectionData(m_shx, m_shy, 0., 0., 0., m_rotz));
+      printf("    m_sh_x = %.3f, m_sh_y = %.3f, m_sh_z = %.3f, m_rot_x = %.3f, m_rot_y = %.3f, m_rot_z = %.3f\n",
+        m_sh_x, m_sh_y, m_sh_z, m_rot_x, m_rot_y, m_rot_z);
+
+      factoredAlignments.addRPCorrection(rpId, RPAlignmentCorrectionData(m_sh_x, m_sh_y, m_sh_z, m_rot_x, m_rot_y, m_rot_z));
 
       // calculate residuals
       for (const auto &senId : sensors)
       {
         factoredAlignments.addSensorCorrection(senId, RPAlignmentCorrectionData(
-          fullAlignments[senId].getShX() - m_shx,
-          fullAlignments[senId].getShY() - m_shy,
-          0.,
-          0.,
-          0.,
-          fullAlignments[senId].getRotZ() - m_rotz
+          fullAlignments[senId].getShX() - m_sh_x,
+          fullAlignments[senId].getShY() - m_sh_y,
+          fullAlignments[senId].getShZ() - m_sh_z,
+          fullAlignments[senId].getRotX() - m_rot_x,
+          fullAlignments[senId].getRotY() - m_rot_y,
+          fullAlignments[senId].getRotZ() - m_rot_z
         ));
       }
     }
